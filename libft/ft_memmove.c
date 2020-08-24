41fd2ed92d6c410116273543deb7a1b1797c81a5
Robert Klein <rklein@c3r2p7.hive.fi>
2020-08-24 09:34:35 +0300

    libft

---
+++ libft/ft_memmove.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rklein <rklein@student.hive.fi>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/19 17:13:20 by rklein            #+#    #+#             */
+/*   Updated: 2020/08/21 15:30:44 by rklein           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memmove(void *dst, const void *src, size_t len)
+{
+	unsigned char	*d;
+	unsigned char	*s;
+	size_t			i;
+
+	d = (unsigned char*)dst;
+	s = (unsigned char*)src;
+	i = 0;
+	if (d < s)
+		ft_memcpy(dst, src, len);
+	else
+	{
+		while (len > 0)
+		{
+			d[len - 1] = s[len - 1];
+			len--;
+		}
+	}
+	return (dst);
+}
