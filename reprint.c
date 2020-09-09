03e891411439f3f76dd2b156147b7f334ff5c9e9
Robert Klein <rklein@c3r2p7.hive.fi>
2020-09-09 16:16:29 +0300

    handles multiline history with linebreaks

--- reprint.c
+++ reprint.c
@@ -6,7 +6,7 @@
 /*   By: rklein <rklein@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/01 12:51:07 by rklein            #+#    #+#             */
-/*   Updated: 2020/09/08 16:32:24 by rklein           ###   ########.fr       */
+/*   Updated: 2020/09/09 16:14:48 by rklein           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,13 +25,13 @@ static void	ft_print_str(t_sh *sh)
 	}
 }
 
-/*static int ft_count_line(t_sh *sh, int n) //n is index or ft_strlen(buffer);
+static int ft_count_line(t_sh *sh, int n) //n is index or ft_strlen(buffer);
 {
 	int	i;
 	int	count;
 	int row;
 
-	count = 0;
+	count = 1;
 	row = 0;
 	i = 0;
 	while (sh->in->buffer[i] && i < n)
@@ -40,24 +40,51 @@ static void	ft_print_str(t_sh *sh)
 				(count + sh->in->prompt_size) % sh->ws.ws_col == 0)
 		{
 			row++;
-			count = 0;
+			//count = 0;
+			count = 0 - sh->in->prompt_size; //NEW
 		}
 		count++;
 		i++;
 	}
 	return (row);
-}*/
+}
 
-static void	ft_cursor(t_sh *sh, int len)
+static void	ft_cursor(t_sh *sh/*, int len*/)
 {
 	int	row;
 	int	cursor;
+	int	i;
 
-	row = len / sh->ws.ws_col;
+//	row = len / sh->ws.ws_col;
+	row = ft_count_line(sh, ft_strlen(sh->in->buffer) - 1);// NEW
 	tputs(tgetstr("cr", NULL), 1, ft_putint);
 	while (row--)
 		tputs(tgetstr("up", NULL), 1, ft_putint);
-	cursor = -1;
+	cursor = 0;//NEW
+	i = 0 - sh->in->prompt_size; //NEW
+	/*
+	** NEW
+	*/
+	while (/*sh->in->buffer[i] &&*/ i < sh->in->index)
+	{
+		if ((i >= 0 && sh->in->buffer[i] == '\n') ||
+				(cursor / sh->ws.ws_col && cursor % sh->ws.ws_col == 0))
+		{
+			tputs(tgetstr("cr", NULL), 1, ft_putint);
+			tputs(tgetstr("do", NULL), 1, ft_putint);
+			cursor = 0;
+		}
+		if (!(i >= 0 && sh->in->buffer[i] == '\n'))
+			tputs(tgetstr("nd", NULL), 1, ft_putint);
+		cursor++;
+		i++;
+	}
+	if (cursor / sh->ws.ws_col && cursor % sh->ws.ws_col == 0)
+	{
+	   tputs(tgetstr("cr", NULL), 1, ft_putint);
+	   tputs(tgetstr("do", NULL), 1, ft_putint);
+	}
+	/*cursor = -1;
 	while (++cursor < sh->in->index + sh->in->prompt_size)
 	{
 		if (cursor / sh->ws.ws_col && cursor % sh->ws.ws_col == 0)
@@ -71,7 +98,7 @@ static void	ft_cursor(t_sh *sh, int len)
 	{
 	   tputs(tgetstr("cr", NULL), 1, ft_putint);
 	   tputs(tgetstr("do", NULL), 1, ft_putint);
-	}
+	}*/
 }
 
 void	ft_reprint(t_sh *sh)
@@ -79,7 +106,7 @@ void	ft_reprint(t_sh *sh)
 	int	len;
 	int	row;
 
-	len = sh->in->prompt_size + ft_strlen(sh->in->buffer) - 1;//not needed if doing line count in cursor ft
+//	len = sh->in->prompt_size + ft_strlen(sh->in->buffer) - 1;//not needed if doing line count in cursor ft
 	row = sh->in->line;
 	tputs(tgetstr("cr", NULL), 1, ft_putint);
 	while (row-- /*&& len > sh->ws.ws_col * row*/)
@@ -87,9 +114,9 @@ void	ft_reprint(t_sh *sh)
 	tputs(tgetstr("cd", NULL), 1, ft_putint);
 	ft_putstr_fd(sh->in->prompt, STDOUT_FILENO);	
 	//ft_putstr_fd(sh->in->buffer, STDOUT_FILENO);//function to print out newlines
-	ft_print_str(sh);
-	ft_cursor(sh, len);
-	sh->in->line = (sh->in->index + sh->in->prompt_size) / sh->ws.ws_col;//line count function that takes into account newlines
-	//ft_count_line(sh)
+	ft_print_str(sh); //NEW
+	ft_cursor(sh/*, len*/);
+//	sh->in->line = (sh->in->index + sh->in->prompt_size) / sh->ws.ws_col;//line count function that takes into account newlines
+	sh->in->line = ft_count_line(sh, sh->in->index); //NEW
 }
 
